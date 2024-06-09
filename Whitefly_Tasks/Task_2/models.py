c104450944c64e059b63e808f2e30e6156866a63
Uzytkownik <uzytkownik@kursGIT.pl>
2024-06-09 12:18:23 +0200

    Task 2 test solution

---
+++ Whitefly_Tasks/Task_2/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(120), nullable=False, unique=True)
+    phone_number = Column(String(20), nullable=False)
